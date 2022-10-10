0cb4dd93cee4b99207b701039147fbfe1941e34a
icezhuhzu <294599659@qq.com>
2022-10-10 12:26:11 +0800

    #0000 v2 bundles相关内容

--- script/SkinBundles.py
+++ script/SkinBundles.py
@@ -6,21 +6,34 @@ _BundlesURLJSONPath = "../json/bundles.json"
 
 def downLoadAllBundlesIcon(data):
     for item in data:
-        if item['displayIcon'] or item['displayIcon2']:
-            if item['displayName']:
-                dir_path = '../res/bundles/' + item['displayName']
-                if not os.path.exists(dir_path):
-                    os.makedirs(dir_path, True)
-            
+        if item['displayName']:
+            dir_path = '../res/bundles/' + item['displayName']
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path, True)
+
+        if item['displayIcon']:
             pic_name = "../res/bundles/" + item['displayName'] + '/' + item['uuid'] + ".jpg"
-            if os.path.exists(pic_name):
-                {}
-            else:
-                pic = Tools.getContentByUrl(item['displayIcon'] or item['displayIcon'], Tools._TIMEOUT)
-                if pic.content:
-                    fp = open(pic_name, 'wb')
-                    fp.write(pic.content)
-                    fp.close()
+            downLoadPic(item['displayIcon'], pic_name)
+
+        if item['displayIcon2']:
+            pic_name = "../res/bundles/" + item['displayName'] + '/' + item['uuid'] + "_displayIcon2" + ".jpg"
+            downLoadPic(item['displayIcon2'], pic_name)
+
+def downLoadPic(url, pic_path):
+    if not url:
+        return
+
+    if not pic_path:
+        return
+
+    if os.path.exists(pic_path):
+        {}
+    else:
+        pic = Tools.getContentByUrl(url, Tools._TIMEOUT)
+        if pic.content:
+            fp = open(pic_path, "wb")
+            fp.write(pic.content)
+            fp.close()
 
 def start():
     json_data = Tools.getJSONDataByUrl(_BundlesURL)
